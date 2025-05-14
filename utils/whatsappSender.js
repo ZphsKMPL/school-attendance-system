9d2cd604294270547b33fdb586c1e8c6d7ed8ac8
ZphsKMPL <zphskondamallepallyattendance@gmail.com>
2025-05-14 22:22:46 +0530

    Create whatsappSender.js

---
+++ utils/whatsappSender.js
@@ -0,0 +1,6 @@
+// Placeholder for integrating WhatsApp API (e.g., Twilio/WhatsApp Business API)
+const sendWhatsAppMessage = (phone, message) => {
+  // Implement API call to send WhatsApp messages
+};
+
+module.exports = sendWhatsAppMessage;
